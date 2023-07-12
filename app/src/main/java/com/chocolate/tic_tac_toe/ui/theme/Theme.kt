bb755516cf52f10dadab8089f3d25be7676d341c
Elboray <ma5126@fayoum.edu.eg>
2023-07-12 08:58:53 +0300

    add DarkColorScheme , OnDarkCustomColorsPalette and CompositionLocalProvider

--- app/src/main/java/com/chocolate/tic_tac_toe/ui/theme/Theme.kt
+++ app/src/main/java/com/chocolate/tic_tac_toe/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.chocolate.tic_tac_toe.ui.theme
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -9,33 +10,46 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
+
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DarkPrimary,
+    onPrimary = DarkOnPrimary,
+    secondary = DarkSecondary,
+    onSecondary = DarkOnSecondary,
+    tertiary = Pink80,
+    background = DarkBackground,
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
+
+val OnDarkCustomColorsPalette = CustomColorsPalette(
+    darkPrimary = DarkPrimary,
+    darkOnPrimary = DarkOnPrimary,
+    darkSecondary = DarkSecondary,
+    darkOnSecondary = DarkOnSecondary,
+    darkOnBackground87 = DarkOnBackground87,
+    darkOnBackground60 = DarkOnBackground60,
+    darkOnBackground38 = DarkOnBackground38,
+    darkCard = DarkCard,
+    darkOnCard = DarkOnCard,
+    darkOnBorder = DarkOnBorder,
+    darkBackground = DarkBackground,
+)
+
+@SuppressLint("CompositionLocalNaming")
+val TicTacCustomColors = staticCompositionLocalOf { CustomColorsPalette() }
 
 @Composable
 fun TicTacToeTheme(
@@ -61,10 +75,15 @@ fun TicTacToeTheme(
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
+    val customColorsPalette = OnDarkCustomColorsPalette
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+    CompositionLocalProvider(
+        TicTacCustomColors provides customColorsPalette
+    ) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
 }
