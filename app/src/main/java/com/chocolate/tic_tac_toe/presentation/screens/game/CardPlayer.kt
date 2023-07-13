660e4c7664f989b08b08af31e99cf31c2d83c96d
Abdallah Sayed <abdullahsayed3355@gmail.com>
2023-07-14 01:12:02 +0300

    refactor players in game screen

---
+++ app/src/main/java/com/chocolate/tic_tac_toe/presentation/screens/game/CardPlayer.kt
@@ -0,0 +1,87 @@
+package com.chocolate.tic_tac_toe.presentation.screens.game
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.chocolate.tic_tac_toe.R
+import com.chocolate.tic_tac_toe.presentation.ui.theme.TicTacCustomColors
+import com.chocolate.tic_tac_toe.presentation.ui.theme.TicTacToeTheme
+
+@Composable
+fun CardPlayer(name: String, score: Int, type: String, image: Int, colorType: Color) {
+    CardPlayerContent(name, score, type, image, colorType)
+}
+
+@Composable
+fun CardPlayerContent(name: String, score: Int, type: String, image: Int, colorType: Color) {
+    TicTacToeTheme() {
+        val color = TicTacCustomColors.current
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 12.dp)
+                .width(118.dp)
+                .height(214.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(color.darkCard), horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Image(
+                painter = painterResource(id = image),
+                contentDescription = null,
+            )
+
+            Text(
+                text = name,
+                style = MaterialTheme.typography.titleMedium,
+                color = color.darkOnBackground87,
+                maxLines = 1,
+                modifier = Modifier.padding(horizontal = 12.dp)
+
+            )
+
+            Text(
+                text = type,
+                color = colorType,
+                style = MaterialTheme.typography.titleLarge
+            )
+            Row {
+                Text(
+                    text = score.toString(),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = TicTacCustomColors.current.darkOnBackground87
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Image(
+                    painter = painterResource(id = R.drawable.fire),
+                    contentDescription = "icon fire",
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .padding(start = 4.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun CardPlayerPreview() {
+
+}
--- app/src/main/java/com/chocolate/tic_tac_toe/presentation/screens/game/Players.kt
+++ app/src/main/java/com/chocolate/tic_tac_toe/presentation/screens/game/Players.kt
@@ -1,23 +1,13 @@
 package com.chocolate.tic_tac_toe.presentation.screens.game
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,7 +16,7 @@ import com.chocolate.tic_tac_toe.presentation.ui.theme.TicTacCustomColors
 import com.chocolate.tic_tac_toe.presentation.ui.theme.TicTacToeTheme
 
 @Composable
-fun players() {
+fun Players() {
     playersContent()
 }
 
@@ -35,103 +25,25 @@ fun players() {
 fun playersContent() {
     TicTacToeTheme() {
         val color = TicTacCustomColors.current
+
         Row(
             modifier = Modifier
-                .height(214.dp)
-                .fillMaxWidth().padding(top=76.dp)
+                .fillMaxWidth()
+                .padding(top = 76.dp)
                 .padding(horizontal = 16.dp), Arrangement.SpaceEvenly
         ) {
-            Column(
-                modifier = Modifier
-                    .width(118.dp)
-                    .height(214.dp)
-                    .clip(RoundedCornerShape(16.dp))
-                    .background(color.darkCard), horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.avatar_batman),
-                    contentDescription = null,
-                    modifier = Modifier
-                )
-
-                Text(
-                    text = "Abooodex3x",
-                    style = MaterialTheme.typography.titleMedium,
-                    color = color.darkOnBackground87
-                )
-
-                Text(
-                    text = "X",
-                    color = color.darkOnSecondary,
-                    style = MaterialTheme.typography.titleLarge
-                )
-                Row {
-                    Text(
-                        text = "300",
-                        style = MaterialTheme.typography.bodyMedium,
-                        color = TicTacCustomColors.current.darkOnBackground87
-                    )
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Image(
-                        painter = painterResource(id = R.drawable.fire),
-                        contentDescription = "icon fire",
-                        modifier = Modifier
-                            .align(Alignment.CenterVertically)
-                            .padding(start = 4.dp)
-                    )
-                }
-            }
-
+            CardPlayer(
+                "aboooodx3x", 300, "O", R.drawable.avatar_batman, color.darkSecondary
+            )
             Text(
                 text = "VS", style = MaterialTheme.typography.bodyMedium,
-                color = TicTacCustomColors.current.darkSecondary,
+                color = TicTacCustomColors.current.darkOnPrimary,
                 textAlign = TextAlign.Justify, modifier = Modifier.padding(top = 214.dp / 2)
             )
 
-
-            Column(
-                modifier = Modifier
-                    .width(118.dp)
-                    .height(214.dp)
-                    .clip(RoundedCornerShape(16.dp))
-                    .background(color.darkCard), horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.clown),
-                    contentDescription = null,
-                    modifier = Modifier
-                )
-
-                Text(
-                    text = "Abooodex3x",
-                    style = MaterialTheme.typography.titleMedium,
-                    color = color.darkOnBackground87
-                )
-
-                Text(
-                    text = "O",
-                    color = color.darkSecondary,
-                    style = MaterialTheme.typography.titleLarge
-                )
-                Row {
-                    Text(
-                        text = "300",
-                        style = MaterialTheme.typography.bodyMedium,
-                        color = TicTacCustomColors.current.darkOnBackground87
-                    )
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Image(
-                        painter = painterResource(id = R.drawable.fire),
-                        contentDescription = "icon fire",
-                        modifier = Modifier
-                            .align(Alignment.CenterVertically)
-                            .padding(start = 4.dp)
-                    )
-                }
-            }
-
+            CardPlayer(
+                "abdallhah3x", 300, "X", R.drawable.clown, color.darkOnSecondary
+            )
 
         }
     }
@@ -141,5 +53,5 @@ fun playersContent() {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun playersPreview() {
-    players()
+    Players()
 }
