3905910cb87df879574b58005628d8e874755181
Mahmoud Elboray <ma5126@fayoum.edu.eg>
2023-07-13 08:31:02 +0300

    add fun TransparentSystemBars

--- app/src/main/java/com/chocolate/tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/chocolate/tic_tac_toe/MainActivity.kt
@@ -3,21 +3,16 @@ package com.chocolate.tic_tac_toe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.chocolate.tic_tac_toe.ui.theme.TicTacToeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             TicTacToeTheme {
-
+                TransparentSystemBars()
             }
         }
     }
