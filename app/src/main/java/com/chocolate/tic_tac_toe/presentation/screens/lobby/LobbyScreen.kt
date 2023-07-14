b1fa5c8feefe21e30e0351d57fc21121a92a8264
Amna <amna.alsikafi@gmail.com>
2023-07-14 16:06:14 +0300

    add header to lobby screen

--- app/src/main/java/com/chocolate/tic_tac_toe/presentation/screens/lobby/LobbyScreen.kt
+++ app/src/main/java/com/chocolate/tic_tac_toe/presentation/screens/lobby/LobbyScreen.kt
@@ -1,21 +1,27 @@
 package com.chocolate.tic_tac_toe.presentation.screens.lobby
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.chocolate.tic_tac_toe.R
 import com.chocolate.tic_tac_toe.presentation.screens.composable.ButtonApp
 import com.chocolate.tic_tac_toe.presentation.screens.composable.SpacerVertical12
 import com.chocolate.tic_tac_toe.presentation.screens.composable.SpacerVertical24
@@ -41,15 +47,18 @@ fun LobbyContent() {
             modifier = Modifier
                 .fillMaxSize()
                 .padding(it)
-                .scrollable(state= scrollState ,orientation = Orientation.Vertical)
+                .scrollable(
+                    state = scrollState, orientation = Orientation.Vertical
+                )
         ) {
-            Header(playerName = "moon")
+            Header()
             SpacerVertical24()
             TopThreePlayers()
             SpacerVertical24()
             Players()
             SpacerVertical24()
-            ButtonApp(text = "Create Game", onClick = { })
+            ButtonApp(text = "Create Game"
+                , onClick = { })
 
         }
     }
@@ -76,37 +85,54 @@ private fun Players() {
 
 
 
-
-
-
-@Composable
-fun Header(playerName: String) {
-    Row() {
-
+    @Composable
+    fun Header() {
+        Column() {
+            PlayerImage()
+        }
+        Column() {
+            PlayerDetails("jinana",3500)
+        }
     }
+
+
+
+
+    @Composable
+    fun PlayerDetails(name:String,score:Int){
+        Row() {
+            Text(text = "player name $name"
+                , modifier = Modifier.padding(start = 8.dp)
+                ,style = TextStyle(
+                    color =DarkOnBackground87 ,
+                    fontSize = 14.sp,
+                    fontFamily = fingerPaintFamily))
+        }
+        Row() {
+            Column() {
+                Text(text = "player score $score"
+                    , modifier = Modifier.padding(start = 8.dp)
+                    ,style = TextStyle(
+                        color =DarkOnBackground38 ,
+                        fontSize = 14.sp,
+                        fontFamily = fingerPaintFamily))
+            }
+            
+            Column() {
+                Image(painter = painterResource(id = R.drawable.ic_fire),
+                    contentDescription =""
+                , modifier = Modifier.size(12.dp)
+                        .padding(start = 4.dp))
+            }
+
+        }
 }
 
-
 @Composable
-fun PlayerDetails(name: String) {
-    Row() {
-        Text(
-            text = "player name $name",
-            modifier = Modifier.padding(start = 8.dp),
-            style = TextStyle(
-                color = DarkOnBackground87, fontSize = 14.sp, fontFamily = fingerPaintFamily
-            )
-        )
-    }
-    Row() {
-        Text(
-            text = "player name $name",
-            modifier = Modifier.padding(start = 8.dp),
-            style = TextStyle(
-                color = DarkOnBackground38, fontSize = 14.sp, fontFamily = fingerPaintFamily
-            )
-        )
-
-    }
-
+fun PlayerImage(){
+    Image(painter = painterResource(id = R.drawable.avatar),
+        contentDescription =""
+        , modifier = Modifier
+            .clip(CircleShape)
+            .size(36.dp))
 }
