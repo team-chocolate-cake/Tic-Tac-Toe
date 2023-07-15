585e61112c403b50d5499339ec2dbe9dbef425f1
jinan <jinanaqb@gmail.com>
2023-07-15 21:52:36 +0300

    get players

--- app/src/main/java/com/chocolate/tic_tac_toe/data/repository/GameRepository.kt
+++ app/src/main/java/com/chocolate/tic_tac_toe/data/repository/GameRepository.kt
@@ -1,10 +1,15 @@
 package com.chocolate.tic_tac_toe.data.repository
 
+import com.chocolate.tic_tac_toe.domain.model.Player
+import kotlinx.coroutines.flow.Flow
+
 
 interface GameRepository {
     // region Session
     // endregion
 
     // region Player
+
+     fun getPlayers() : Flow<List<Player?>>
     // endregion
 }
--- app/src/main/java/com/chocolate/tic_tac_toe/data/repository/GameRepositoryImp.kt
+++ app/src/main/java/com/chocolate/tic_tac_toe/data/repository/GameRepositoryImp.kt
@@ -2,6 +2,8 @@ package com.chocolate.tic_tac_toe.data.repository
 
 import com.chocolate.tic_tac_toe.data.remote.FirebasePlayerDatabase
 import com.chocolate.tic_tac_toe.data.remote.FirebaseSessionDatabase
+import com.chocolate.tic_tac_toe.domain.model.Player
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GameRepositoryImp @Inject constructor(
@@ -13,6 +15,10 @@ class GameRepositoryImp @Inject constructor(
     // endregion
 
     // region Player
+    override  fun getPlayers(): Flow<List<Player?>> {
+        return firebasePlayerDatabase.getPlayers()
+    }
+    //endregion
 
     // endregion
 }
