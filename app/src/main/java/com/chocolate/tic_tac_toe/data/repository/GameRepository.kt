fc5b397a6e3f71d43e0fad587b2deeab6020223d
Mahmoud Elboray <ma5126@fayoum.edu.eg>
2023-07-15 18:03:44 +0300

    add fun getBoard in GameRepository

--- app/src/main/java/com/chocolate/tic_tac_toe/data/repository/GameRepository.kt
+++ app/src/main/java/com/chocolate/tic_tac_toe/data/repository/GameRepository.kt
@@ -1,12 +1,16 @@
 package com.chocolate.tic_tac_toe.data.repository
 
+import com.chocolate.tic_tac_toe.domain.model.Session
+import kotlinx.coroutines.flow.Flow
+
 
 interface GameRepository {
     // region Session
 
     fun updateBoard(board: List<List<String>>, key: String)
-    // endregion
 
+    // endregion
+    fun getBoard(key: String): Flow<Session?>
     // region Player
     // endregion
 }
