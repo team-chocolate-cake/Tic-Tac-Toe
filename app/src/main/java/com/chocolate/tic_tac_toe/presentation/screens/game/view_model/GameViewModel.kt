0802a8c17bec6775b0fc7acd1832d4e708b86cbd
Mahmoud Elboray <ma5126@fayoum.edu.eg>
2023-07-15 18:07:44 +0300

    add fun getBoard in viewModel

---
+++ app/src/main/java/com/chocolate/tic_tac_toe/presentation/screens/game/view_model/GameViewModel.kt
@@ -0,0 +1,46 @@
+package com.chocolate.tic_tac_toe.presentation.screens.game.view_model
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.chocolate.tic_tac_toe.data.repository.GameRepository
+import com.chocolate.tic_tac_toe.presentation.screens.game.GameUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class GameViewModel @Inject constructor(
+    private val gameRepository: GameRepository,
+) : ViewModel() {
+    private val _gameUiState = MutableStateFlow<GameUiState?>(null)
+    val gameUiState = _gameUiState.asStateFlow()
+
+    init {
+        updateBoard()
+        getBoard()
+    }
+
+    private fun updateBoard() {
+        gameRepository.updateBoard(
+            listOf(listOf("X", "O", "X"), listOf("X", "O", "X"), listOf("X", "O", "X")),
+            key
+        )
+    }
+
+    private fun getBoard() {
+        viewModelScope.launch {
+            gameRepository.getBoard(key).collect { session ->
+                val board = session?.board
+                val updatedGameUiState = GameUiState(board = board ?: emptyList())
+                _gameUiState.value = updatedGameUiState
+            }
+        }
+    }
+
+    companion object {
+        private const val key = "-N_OIaJqsm7zT_Ik3TPp"
+    }
+}
