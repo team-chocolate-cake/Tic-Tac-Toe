b88a0cbc7c25cf5b3588708bb670b3566767b27c
jinan <jinanaqb@gmail.com>
2023-07-16 22:28:59 +0300

    add view model to players

--- app/src/main/java/com/chocolate/tic_tac_toe/presentation/screens/lobby/viewmodel/LobbyUiState.kt
+++ app/src/main/java/com/chocolate/tic_tac_toe/presentation/screens/lobby/viewmodel/LobbyUiState.kt
@@ -3,14 +3,17 @@ package com.chocolate.tic_tac_toe.presentation.screens.lobby.viewmodel
 import com.chocolate.tic_tac_toe.domain.model.Player
 
 data class LobbyUiState(
-    val players: List<Player> = emptyList(),
-    val error : String = "",
+    val players: List<Player?> = emptyList(),
+    val player: Player? = null,
+    val error: String? = null,
     val isEmpty: Boolean = false,
-){
-    data class PlayerUiState(
-        val name: String ="",
-        val score: Int = 0,
-        val createdSessionId: String = "",
-        val imageUrl: String = ""
-    )
-}
+    val isLoading: Boolean = false,
+)
+
+data class PlayerUiState(
+    val id: String = "",
+    val name: String = "",
+    val score: Int = 0,
+    val createdSessionId: String = "",
+    val imageUrl: String = ""
+)
---
+++ app/src/main/java/com/chocolate/tic_tac_toe/presentation/screens/lobby/viewmodel/LobbyViewModel.kt
@@ -0,0 +1,127 @@
+package com.chocolate.tic_tac_toe.presentation.screens.lobby.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.chocolate.tic_tac_toe.domain.model.Session
+import com.chocolate.tic_tac_toe.domain.usecase.lobby.GetPlayerByIdUseCase
+import com.chocolate.tic_tac_toe.domain.usecase.lobby.GetPlayersUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+@HiltViewModel
+class LobbyViewModel(
+    private val getPlayerUseCase: GetPlayerByIdUseCase,
+    private val getPlayersUseCase: GetPlayersUseCase,
+) : ViewModel() , LobbyListener {
+
+    private val _state = MutableStateFlow(LobbyUiState())
+    val state = _state.asStateFlow()
+
+    init {
+        getPlayer(id = "")
+        getPlayers()
+    }
+
+    private fun getPlayer(id: String) {
+        viewModelScope.launch {
+            getPlayerUseCase(id = id).collect { player ->
+                _state.update {
+                    it.copy(
+                        player = player,
+                        isLoading = false,
+                        error = null,
+                    )
+                }
+            }
+        }
+    }
+
+    private fun getPlayers() {
+
+        viewModelScope.launch {
+            getPlayersUseCase().collect { players ->
+                _state.update {
+                    it.copy(
+                        players = players,
+                        isLoading = false,
+                        error = null,
+                    )
+                }
+            }
+        }
+    }
+
+    override fun onClickPlayer(sessionId: String) {
+
+    }
+
+    override fun onClickCreateSession(session: Session) {
+
+    }
+
+
+//
+//    private fun getPlayer(id: String){
+//        _state.update { it.copy(isLoading = true) }
+//        tryToExecute(
+//            call = {
+//                getPlayerUseCase(id)
+//            },
+//            onSuccess = { onGetPlayerSuccess() },
+//            onError = ::onError
+//        )
+//    }
+//
+//    private fun onGetPlayerSuccess(item: Flow<PlayerUiState>) {
+//        _state.update {
+//            it.copy(
+//                players = item,
+//                isLoading = false,
+//                error = null,
+//            )
+//        }
+//    }
+//
+//    private fun getPlayers(){
+//
+//    }
+
+
+//
+//    private fun onError(throwable: Throwable) {
+//        if (throwable == NoNetworkThrowable()) {
+//            showErrorWithSnackBar(throwable.message ?: "No Network Connection")
+//        } else if (throwable == SocketTimeoutException()) {
+//            showErrorWithSnackBar(throwable.message ?: "time out!")
+//        }
+//        _state.update {
+//            it.copy(
+//                error = throwable.message ?: "No Network Connection",
+//                isLoading = false
+//            )
+//        }
+//    }
+//
+//    private fun showErrorWithSnackBar(messages: String) {
+////        sendEvent(MyListDetailsUiEvent.ShowSnackBar(messages))
+//    }
+//
+//    private fun <T> tryToExecute(
+//        call: () -> Flow<T?>,
+//        onSuccess: (Flow<T?>) -> Unit,
+//        onError: (Throwable) -> Unit,
+//        dispatcher: CoroutineDispatcher = Dispatchers.IO
+//    ) {
+//        viewModelScope.launch(dispatcher) {
+//            try {
+//                call().also(onSuccess)
+//            } catch (th: Throwable) {
+//                onError(th)
+//            }
+//        }
+//    }
+
+}
