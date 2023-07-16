41fc3a9d7462e2d2300a580379af2c09c60f984b
Mahmoud Elboray <ma5126@fayoum.edu.eg>
2023-07-16 15:51:42 +0300

    add CardDraw

---
+++ app/src/main/java/com/chocolate/tic_tac_toe/presentation/screens/game/components/CardDraw.kt
@@ -0,0 +1,88 @@
+package com.chocolate.tic_tac_toe.presentation.screens.game.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.chocolate.tic_tac_toe.R
+import com.chocolate.tic_tac_toe.presentation.ui.theme.TicTacCustomColors
+import com.chocolate.tic_tac_toe.presentation.ui.theme.TicTacToeTheme
+
+@Composable
+fun CardDrawContent(
+    image: Int,
+) {
+    val color = TicTacCustomColors.current
+    Column(
+        modifier = Modifier
+            .clip(RoundedCornerShape(24.dp))
+            .background(color.darkCard)
+            .padding(vertical = 24.dp, horizontal = 32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        Image(
+            modifier = Modifier
+                .width(155.dp)
+                .height(168.dp),
+            painter = painterResource(id = image),
+            contentDescription = null,
+        )
+        Text(
+            text = "Draw Game!",
+            style = MaterialTheme.typography.titleLarge.copy(fontSize = 30.sp),
+            color = color.darkOnBackground87,
+            maxLines = 1,
+
+            )
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "X",
+                style = MaterialTheme.typography.titleLarge.copy(fontSize = 64.sp),
+                color = color.darkOnSecondary,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+            )
+            Text(
+                text = "O",
+                style = MaterialTheme.typography.titleLarge.copy(fontSize = 64.sp),
+                color = color.darkSecondary,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CardPreview() {
+    TicTacToeTheme() {
+        CardDrawContent(
+            image = R.drawable.avatar_batman,
+        )
+    }
+}
