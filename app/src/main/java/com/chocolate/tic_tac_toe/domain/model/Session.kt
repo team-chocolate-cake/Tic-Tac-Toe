19ee179b70ce96e151b92440e965cb2023ef2e02
Amna <amna.alsikafi@gmail.com>
2023-07-16 23:30:23 +0300

    add create session in view model

--- app/src/main/java/com/chocolate/tic_tac_toe/domain/model/Session.kt
+++ app/src/main/java/com/chocolate/tic_tac_toe/domain/model/Session.kt
@@ -2,10 +2,10 @@ package com.chocolate.tic_tac_toe.domain.model
 
 data class Session(
     val id: String,
-    val players: Pair<Player, Player>,
-    val board: List<List<String>>,
-    val turn: Player,
-    val winner: Player?,
-    val state: Boolean,
-    val playerRequest: Player?
+    val players: Pair<Player, Player>?=null,
+    val board: List<List<String>>?=null,
+    val turn: Player?=null,
+    val winner: Player?=null,
+    val state: Boolean?=null,
+    val playerRequest: Player?=null
 )
--- app/src/main/java/com/chocolate/tic_tac_toe/presentation/screens/lobby/viewmodel/LobbyViewModel.kt
+++ app/src/main/java/com/chocolate/tic_tac_toe/presentation/screens/lobby/viewmodel/LobbyViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import com.chocolate.tic_tac_toe.domain.model.Session
 import com.chocolate.tic_tac_toe.domain.usecase.lobby.GetPlayerByIdUseCase
 import com.chocolate.tic_tac_toe.domain.usecase.lobby.GetPlayersUseCase
+import com.chocolate.tic_tac_toe.domain.usecase.session.CreateSessionUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -15,6 +16,7 @@ import kotlinx.coroutines.launch
 class LobbyViewModel(
     private val getPlayerUseCase: GetPlayerByIdUseCase,
     private val getPlayersUseCase: GetPlayersUseCase,
+    private val createSessionUseCase: CreateSessionUseCase
 ) : ViewModel() , LobbyListener {
 
     private val _state = MutableStateFlow(LobbyUiState())
@@ -59,6 +61,11 @@ class LobbyViewModel(
     }
 
     override fun onClickCreateSession(session: Session) {
+        _state.update { it.copy(isLoading = true) }
+        viewModelScope.launch {
+            createSessionUseCase(session)
+            _state.update { it.copy(isLoading = false) }
+        }
 
     }
 
