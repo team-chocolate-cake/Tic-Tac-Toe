70b478d3d05a074c5e3555844a97e78de540188f
Abdelrahman Sedky <46361405+Abdelrahman-Sedky@users.noreply.github.com>
2023-07-17 02:15:21 +0300

    update edge to edge

--- app/src/main/java/com/chocolate/tic_tac_toe/presentation/screens/composable/TransparentSystemBars.kt
+++ app/src/main/java/com/chocolate/tic_tac_toe/presentation/screens/composable/TransparentSystemBars.kt
@@ -1,8 +1,8 @@
-package com.chocolate.tic_tac_toe
+package com.chocolate.tic_tac_toe.presentation.screens.composable
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
@@ -11,12 +11,9 @@ fun TransparentSystemBars() {
     val systemUiController = rememberSystemUiController()
     val useDarkIcons = !isSystemInDarkTheme()
 
-    DisposableEffect(systemUiController, useDarkIcons) {
+    LaunchedEffect(systemUiController, useDarkIcons) {
         systemUiController.setSystemBarsColor(
             color = Color.Transparent,
-            darkIcons = useDarkIcons
         )
-
-        onDispose {}
     }
 }
