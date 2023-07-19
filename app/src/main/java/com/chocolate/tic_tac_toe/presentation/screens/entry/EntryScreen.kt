9d992f430931745f0185c5683a7f5771797b8783
mohammed-akkad <momoo362514@gmail.com>
2023-07-19 19:32:10 +0300

    cleanup

--- app/src/main/java/com/chocolate/tic_tac_toe/presentation/screens/entry/EntryScreen.kt
+++ app/src/main/java/com/chocolate/tic_tac_toe/presentation/screens/entry/EntryScreen.kt
@@ -1,12 +1,10 @@
 package com.chocolate.tic_tac_toe.presentation.screens.entry
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
